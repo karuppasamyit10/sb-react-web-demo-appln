5d25cdb59e6550de20053825920e14ab515ad77c
karuppasamyit10 <karuppasamyit10@gmail.com>
2019-08-27 19:38:45 +0530

    Gitisgore, build grade tasks added, user identity added

--- src/main/java/com/example/entity/User.java
+++ src/main/java/com/example/entity/User.java
@@ -25,7 +25,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 public class User implements Serializable {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "user_id")
 	private long userId;
 
