9977a11568d4c0fd13394f654a3213795f8216b0
Karuppasamyit10 <Karuppasamyit10@gmail.com>
2019-12-06 16:34:36 +0530

    multilanuage support  basic added

--- react-web/src/components/public/Header.js
+++ react-web/src/components/public/Header.js
@@ -20,8 +20,16 @@ class Header extends Component {
     super(props);
     this.state = {
       token: null,
-      open: false,
-      languageCode: "EN"
+      open : false,
+      error: null,
+      isLoaded: false,
+      items: [],
+      Menu1:"",
+      Menu2:"",
+      Menu3:"",
+      Menu4:"",
+      Menu5:"",
+      Menu6:""
     };
   }
 
@@ -29,32 +37,77 @@ class Header extends Component {
     let token = store.get("userSession");
     console.log(token);
     this.setState({ token: token });
+    let jsonFile = require('../../assets/Content.json');
+    this.setState({items:jsonFile});
+    console.log(jsonFile);  
+    console.log(jsonFile.items[0].Menu1);
+    console.log(localStorage.getItem("language"));
+    this.setState.items=jsonFile;
+    if(localStorage.getItem("language")=="ES"){
+      this.setState.Menu1=jsonFile.items[1].Menu1;
+      this.setState.Menu2=jsonFile.items[1].Menu2;
+      this.setState.Menu3=jsonFile.items[1].Menu3;
+      this.setState.Menu4=jsonFile.items[1].Menu4;
+      this.setState.Menu5=jsonFile.items[1].Menu5;
+      this.setState.Menu6=jsonFile.items[1].Menu6;
+    }
+    else{
+      this.setState.Menu1=jsonFile.items[0].Menu1;
+      this.setState.Menu2=jsonFile.items[0].Menu2;
+      this.setState.Menu3=jsonFile.items[1].Menu3;
+      this.setState.Menu4=jsonFile.items[1].Menu4;
+      this.setState.Menu5=jsonFile.items[1].Menu5;
+      this.setState.Menu6=jsonFile.items[1].Menu6;
+    }
+    
   }
 
   handleSignOut = () => {
     this.props.logout(response => {
       console.log(response);
+      this.props.prop.history.push("/");
     });
-    this.setState({ token: null });
-    store.clearAll();
-    this.props.history.push("/");
+  };
+
+  handleLanguage = (languageCode,code) => {
+    //localStorage.setItem('language',languageCode);
+    //alert(localStorage.getItem('language'));   
+    // this.setState.items.forEach(lang,i=>{
+    //   console.log(lang.items[{i}].Menu1);
+    // })
+    //for (let lang of this.items) {
+     //console.log(lang.items[0].Menu1);
+    //}
+    console.log(this.setState.items);
+    var i=0;     
+    for(let [key, value] of Object.entries(this.setState.items))
+    {
+      if(languageCode==value[code].languageid)
+      {
+        localStorage.setItem('language',value[code].languageid);
+        this.setState.Menu1=value[code].Menu1;
+        this.setState.Menu2=value[code].Menu2;
+        window.location.reload();
+       console.log("Language Code Matched") ;
+       console.log(localStorage.getItem('language'));
+      }
+      console.log(value[i].Menu1);
+      i++;
+    }
   };
 
   handleSignIn = () => {
-    this.props.history.push(PATH.SIGIN);
+    this.props.prop.history.push(PATH.SIGIN);
+    console.log(this.items);
   };
 
   render() {
-    let userSession = store.get("userSession");
-    let common = userSession && userSession.common ? userSession.common : null;
-    let languageList =
-      common && common.languageList ? common.languageList : null;
+    
     return (
       <header class="header">
         <nav
           id="navHeader"
-          class="navbar navbar-expand-md navbar-light bg-light fixed-top hideForAni"
-        >
+          class="navbar navbar-expand-md navbar-light bg-light fixed-top hideForAni">
           <div class="row parent-row no-gutters">
             <div class="logo-wrap col-12">
               <div class="container">
@@ -65,7 +118,7 @@ class Header extends Component {
                     style={{ cursor: "pointer" }}
                     alt=""
                     onClick={() => {
-                      this.props.history.push(PATH.DASHBOARD);
+                      this.props.prop.history.push(PATH.DASHBOARD);
                     }}
                   />
                 </a>
@@ -116,13 +169,9 @@ class Header extends Component {
                   <div class="hamburger_wrap d-inline-flex">
                     <button
                       id="hamburg"
-                      class={`hamburger hamburger--collapse ${
-                        this.state.open ? "is-active" : ""
-                      }`}
+                      class={`hamburger hamburger--collapse ${this.state.open ? 'is-active' : ''}`}
                       type="button"
-                      onClick={() => {
-                        this.setState({ open: !this.state.open });
-                      }}
+                      onClick={()=>{this.setState({open : !this.state.open})}}
                     >
                       <span class="hamburger-box">
                         <span class="hamburger-inner"></span>
@@ -132,7 +181,7 @@ class Header extends Component {
                 </div>
               </div>
             </div>
-            <div class={`menu-wrap col-12 ${this.state.open ? "active" : ""}`}>
+            <div class={`menu-wrap col-12 ${this.state.open ? 'active' : ''}`}>
               <div class="menu-languages">
                 <div class="container">
                   <span class="d-inline-block globe">
@@ -141,39 +190,44 @@ class Header extends Component {
                     </a>
                   </span>
                   <ul class="language-links d-inline-block">
-                    {languageList && languageList.length
-                      ? languageList.map((language, i) => {
-                          return (
-                            <li>
-                              <a
-                                href="javascript:;"
-                                class={
-                                  this.state.languageCode ===
-                                  language.languageCode
-                                    ? "active"
-                                    : ""
-                                }
-                              >
-                                {language.language}
-                              </a>
-                            </li>
-                          );
-                        })
-                      : ""}
+                    <li>
+                      <a onClick={() => {
+                          this.handleLanguage("EN",0);
+                       }} class="active">
+                        English
+                      </a>
+                    </li>
+                    <li>
+                      <a onClick={()=>{this.handleLanguage("ES",1);}} href="javascript:;">Español</a>
+                    </li>
+                    <li>
+                      <a onClick={()=>{this.handleLanguage("FR",2);}} href="javascript:;">Français</a>
+                    </li>
+                    <li>
+                      <a onClick={()=>{this.handleLanguage("AR",3);}} href="javascript:;">العربية</a>
+                    </li>
+                    <li>
+                      <a href="javascript:;">ქართული</a>
+                    </li>
+                    <li>
+                      <a href="javascript:;">Kiswahili</a>
+                    </li>
+                    <li>
+                      <a href="javascript:;">русский</a>
+                    </li>
+                    <li>
+                      <a href="javascript:;">Português </a>
+                    </li>
+                    <li>
+                      <a onClick={()=>{this.handleLanguage("ES");}} href="javascript:;">日本語</a>
+                    </li>
+                    <li>
+                      <a href="javascript:;">中文 </a>
+                    </li>
+                    <li>
+                      <a href="javascript:;">한국어</a>
+                    </li>
                   </ul>
-                  <select class="language_select">
-                    <option value="0">English</option>
-                    <option value="1">Español</option>
-                    <option value="2">Français</option>
-                    <option value="3">العربية</option>
-                    <option value="4">ქართული</option>
-                    <option value="5">Kiswahili</option>
-                    <option value="6">русский</option>
-                    <option value="7">Português</option>
-                    <option value="8">日本語</option>
-                    <option value="9">中文</option>
-                    <option value="10">한국어</option>
-                  </select>
                 </div>
               </div>
               <div class="menu-main">
@@ -182,33 +236,33 @@ class Header extends Component {
                     <ul class="navbar-nav nav-center mr-auto">
                       <li class="nav-item active">
                         <Link class="nav-link" to={PATH.ADVANCED_SEARCH}>
-                          Car Shop
+                        {this.setState.Menu1}
                         </Link>
                       </li>
                       <li class="nav-item">
-                        <Link class="nav-link" to={PATH.HOW_TO_ORDER}>
-                          How to Order
-                        </Link>
+                        <a class="nav-link" href="#">
+                        {this.setState.Menu2}
+                        </a>
                       </li>
                       <li class="nav-item">
-                        <Link class="nav-link" to={PATH.transport}>
-                          Transport
-                        </Link>
+                        <a class="nav-link" href="#">
+                        {this.setState.Menu3}
+                        </a>
                       </li>
                       <li class="nav-item">
-                        <Link class="nav-link" to={PATH.PARTNER}>
-                          Partner
-                        </Link>
+                        <a class="nav-link" href="#">
+                        {this.setState.Menu4}
+                        </a>
                       </li>
                       <li class="nav-item">
-                        <Link class="nav-link" to={PATH.SELLER}>
-                          Seller
-                        </Link>
+                        <a class="nav-link" href="#">
+                        {this.setState.Menu5}
+                        </a>
                       </li>
                       <li class="nav-item">
-                        <Link class="nav-link" to={PATH.COMMUNITY}>
-                          Community
-                        </Link>
+                        <a class="nav-link" href="#">
+                        {this.setState.Menu6}
+                        </a>
                       </li>
                     </ul>
                     <ul class="navbar-nav nav-right">
@@ -244,13 +298,12 @@ class Header extends Component {
                           My Account
                         </a>
                         <div class="dropdown-menu" aria-labelledby="dropdown01">
-                          <Link
+                          <a
                             class="dropdown-item"
                             style={{ cursor: "pointer" }}
-                            to={PATH.SAVED_SEARCH}
                           >
                             Saved Searches
-                          </Link>
+                          </a>
                           <a
                             class="dropdown-item"
                             style={{ cursor: "pointer" }}
@@ -314,4 +367,7 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-export default connect(null, mapDispatchToProps)(Header);
+export default connect(
+  null,
+  mapDispatchToProps
+)(Header);
--- react-web/src/components/public/Header_2.js
+++ react-web/src/components/public/Header_2.js
@@ -20,16 +20,8 @@ class Header extends Component {
     super(props);
     this.state = {
       token: null,
-      open : false,
-      error: null,
-      isLoaded: false,
-      items: [],
-      Menu1:"",
-      Menu2:"",
-      Menu3:"",
-      Menu4:"",
-      Menu5:"",
-      Menu6:""
+      open: false,
+      languageCode: "EN"
     };
   }
 
@@ -37,77 +29,32 @@ class Header extends Component {
     let token = store.get("userSession");
     console.log(token);
     this.setState({ token: token });
-    let jsonFile = require('../../assets/Content.json');
-    this.setState({items:jsonFile});
-    console.log(jsonFile);  
-    console.log(jsonFile.items[0].Menu1);
-    console.log(localStorage.getItem("language"));
-    this.setState.items=jsonFile;
-    if(localStorage.getItem("language")=="ES"){
-      this.setState.Menu1=jsonFile.items[1].Menu1;
-      this.setState.Menu2=jsonFile.items[1].Menu2;
-      this.setState.Menu3=jsonFile.items[1].Menu3;
-      this.setState.Menu4=jsonFile.items[1].Menu4;
-      this.setState.Menu5=jsonFile.items[1].Menu5;
-      this.setState.Menu6=jsonFile.items[1].Menu6;
-    }
-    else{
-      this.setState.Menu1=jsonFile.items[0].Menu1;
-      this.setState.Menu2=jsonFile.items[0].Menu2;
-      this.setState.Menu3=jsonFile.items[1].Menu3;
-      this.setState.Menu4=jsonFile.items[1].Menu4;
-      this.setState.Menu5=jsonFile.items[1].Menu5;
-      this.setState.Menu6=jsonFile.items[1].Menu6;
-    }
-    
   }
 
   handleSignOut = () => {
     this.props.logout(response => {
       console.log(response);
-      this.props.prop.history.push("/");
     });
-  };
-
-  handleLanguage = (languageCode,code) => {
-    //localStorage.setItem('language',languageCode);
-    //alert(localStorage.getItem('language'));   
-    // this.setState.items.forEach(lang,i=>{
-    //   console.log(lang.items[{i}].Menu1);
-    // })
-    //for (let lang of this.items) {
-     //console.log(lang.items[0].Menu1);
-    //}
-    console.log(this.setState.items);
-    var i=0;     
-    for(let [key, value] of Object.entries(this.setState.items))
-    {
-      if(languageCode==value[code].languageid)
-      {
-        localStorage.setItem('language',value[code].languageid);
-        this.setState.Menu1=value[code].Menu1;
-        this.setState.Menu2=value[code].Menu2;
-        window.location.reload();
-       console.log("Language Code Matched") ;
-       console.log(localStorage.getItem('language'));
-      }
-      console.log(value[i].Menu1);
-      i++;
-    }
+    this.setState({ token: null });
+    store.clearAll();
+    this.props.history.push("/");
   };
 
   handleSignIn = () => {
-    this.props.prop.history.push(PATH.SIGIN);
-    console.log(this.items);
+    this.props.history.push(PATH.SIGIN);
   };
 
   render() {
-    
+    let userSession = store.get("userSession");
+    let common = userSession && userSession.common ? userSession.common : null;
+    let languageList =
+      common && common.languageList ? common.languageList : null;
     return (
       <header class="header">
         <nav
           id="navHeader"
-          class="navbar navbar-expand-md navbar-light bg-light fixed-top hideForAni">
+          class="navbar navbar-expand-md navbar-light bg-light fixed-top hideForAni"
+        >
           <div class="row parent-row no-gutters">
             <div class="logo-wrap col-12">
               <div class="container">
@@ -118,7 +65,7 @@ class Header extends Component {
                     style={{ cursor: "pointer" }}
                     alt=""
                     onClick={() => {
-                      this.props.prop.history.push(PATH.DASHBOARD);
+                      this.props.history.push(PATH.DASHBOARD);
                     }}
                   />
                 </a>
@@ -169,9 +116,13 @@ class Header extends Component {
                   <div class="hamburger_wrap d-inline-flex">
                     <button
                       id="hamburg"
-                      class={`hamburger hamburger--collapse ${this.state.open ? 'is-active' : ''}`}
+                      class={`hamburger hamburger--collapse ${
+                        this.state.open ? "is-active" : ""
+                      }`}
                       type="button"
-                      onClick={()=>{this.setState({open : !this.state.open})}}
+                      onClick={() => {
+                        this.setState({ open: !this.state.open });
+                      }}
                     >
                       <span class="hamburger-box">
                         <span class="hamburger-inner"></span>
@@ -181,7 +132,7 @@ class Header extends Component {
                 </div>
               </div>
             </div>
-            <div class={`menu-wrap col-12 ${this.state.open ? 'active' : ''}`}>
+            <div class={`menu-wrap col-12 ${this.state.open ? "active" : ""}`}>
               <div class="menu-languages">
                 <div class="container">
                   <span class="d-inline-block globe">
@@ -190,44 +141,39 @@ class Header extends Component {
                     </a>
                   </span>
                   <ul class="language-links d-inline-block">
-                    <li>
-                      <a onClick={() => {
-                          this.handleLanguage("EN",0);
-                       }} class="active">
-                        English
-                      </a>
-                    </li>
-                    <li>
-                      <a onClick={()=>{this.handleLanguage("ES",1);}} href="javascript:;">Español</a>
-                    </li>
-                    <li>
-                      <a onClick={()=>{this.handleLanguage("FR",2);}} href="javascript:;">Français</a>
-                    </li>
-                    <li>
-                      <a onClick={()=>{this.handleLanguage("AR",3);}} href="javascript:;">العربية</a>
-                    </li>
-                    <li>
-                      <a href="javascript:;">ქართული</a>
-                    </li>
-                    <li>
-                      <a href="javascript:;">Kiswahili</a>
-                    </li>
-                    <li>
-                      <a href="javascript:;">русский</a>
-                    </li>
-                    <li>
-                      <a href="javascript:;">Português </a>
-                    </li>
-                    <li>
-                      <a onClick={()=>{this.handleLanguage("ES");}} href="javascript:;">日本語</a>
-                    </li>
-                    <li>
-                      <a href="javascript:;">中文 </a>
-                    </li>
-                    <li>
-                      <a href="javascript:;">한국어</a>
-                    </li>
+                    {languageList && languageList.length
+                      ? languageList.map((language, i) => {
+                          return (
+                            <li>
+                              <a
+                                href="javascript:;"
+                                class={
+                                  this.state.languageCode ===
+                                  language.languageCode
+                                    ? "active"
+                                    : ""
+                                }
+                              >
+                                {language.language}
+                              </a>
+                            </li>
+                          );
+                        })
+                      : ""}
                   </ul>
+                  <select class="language_select">
+                    <option value="0">English</option>
+                    <option value="1">Español</option>
+                    <option value="2">Français</option>
+                    <option value="3">العربية</option>
+                    <option value="4">ქართული</option>
+                    <option value="5">Kiswahili</option>
+                    <option value="6">русский</option>
+                    <option value="7">Português</option>
+                    <option value="8">日本語</option>
+                    <option value="9">中文</option>
+                    <option value="10">한국어</option>
+                  </select>
                 </div>
               </div>
               <div class="menu-main">
@@ -236,33 +182,33 @@ class Header extends Component {
                     <ul class="navbar-nav nav-center mr-auto">
                       <li class="nav-item active">
                         <Link class="nav-link" to={PATH.ADVANCED_SEARCH}>
-                        {this.setState.Menu1}
+                          Car Shop
                         </Link>
                       </li>
                       <li class="nav-item">
-                        <a class="nav-link" href="#">
-                        {this.setState.Menu2}
-                        </a>
+                        <Link class="nav-link" to={PATH.HOW_TO_ORDER}>
+                          How to Order
+                        </Link>
                       </li>
                       <li class="nav-item">
-                        <a class="nav-link" href="#">
-                        {this.setState.Menu3}
-                        </a>
+                        <Link class="nav-link" to={PATH.transport}>
+                          Transport
+                        </Link>
                       </li>
                       <li class="nav-item">
-                        <a class="nav-link" href="#">
-                        {this.setState.Menu4}
-                        </a>
+                        <Link class="nav-link" to={PATH.PARTNER}>
+                          Partner
+                        </Link>
                       </li>
                       <li class="nav-item">
-                        <a class="nav-link" href="#">
-                        {this.setState.Menu5}
-                        </a>
+                        <Link class="nav-link" to={PATH.SELLER}>
+                          Seller
+                        </Link>
                       </li>
                       <li class="nav-item">
-                        <a class="nav-link" href="#">
-                        {this.setState.Menu6}
-                        </a>
+                        <Link class="nav-link" to={PATH.COMMUNITY}>
+                          Community
+                        </Link>
                       </li>
                     </ul>
                     <ul class="navbar-nav nav-right">
@@ -298,12 +244,13 @@ class Header extends Component {
                           My Account
                         </a>
                         <div class="dropdown-menu" aria-labelledby="dropdown01">
-                          <a
+                          <Link
                             class="dropdown-item"
                             style={{ cursor: "pointer" }}
+                            to={PATH.SAVED_SEARCH}
                           >
                             Saved Searches
-                          </a>
+                          </Link>
                           <a
                             class="dropdown-item"
                             style={{ cursor: "pointer" }}
@@ -367,7 +314,4 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-export default connect(
-  null,
-  mapDispatchToProps
-)(Header);
+export default connect(null, mapDispatchToProps)(Header);
